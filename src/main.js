3b1e7e543150e0479aff58b874fe76cda3ea9a4a
赵磊 <zhaolei@zhaoleideMacBook-Pro.local>
2019-01-10 16:42:25 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+//import './styles/element-variables.scss'
 
 import store from '@/store/index.js'
 
