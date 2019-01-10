b33e58e805dcca48c04300418cba80a2094a2c7b
赵磊 <zhaolei@zhaoleideMacBook-Pro.local>
2019-01-10 08:13:02 +0800

    1

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -18,6 +18,10 @@ export default {
      * 当前窗体大小类型，STYLE_MOBILE/STYLE_DESKTOP,STYLE_DESKTOP
      */
     windowsSizeStyle: 'STYLE_DESKTOP',
+    /**
+     * 控件大小 medium/small/mini
+     */
+    controlSize: 'medium',
   },
   mutations: {
     /**
@@ -48,6 +52,12 @@ export default {
      */
     SET_WINDOWS_SIZE_STYLE: (state, style) => {
       state.windowsSizeStyle = style
+    },
+    /**
+     * 设置控件大小
+     */
+    SET_CONTROL_SIZE: (state, size) => {
+      state.controlSize = size
     },
   },
   actions: {},
