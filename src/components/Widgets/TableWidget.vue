1a51d325383242115db65224b94c45f0ef0b80aa
赵磊 <zhaolei@zhaoleideMacBook-Pro.local>
2019-05-17 08:25:43 +0800

    1

--- src/components/Widgets/TableWidget.vue
+++ src/components/Widgets/TableWidget.vue
@@ -444,10 +444,11 @@ export default {
 
 <style scoped>
 .mainwidget {
-  height: 100%;
+  /* height: 100%; */
+  /* padding: 5px 5px 5px 5px; */
 }
 .toolbuttongroup {
-  padding: 5px 10px 5px 10px;
+  padding: 10px 10px 5px 10px;
 }
 .simplefilter {
   padding: 5px 10px 5px 10px;
